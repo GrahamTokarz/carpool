04e5e2ce8144e15e6b99ad74e7c52e10a6ea75f8
Bret Jones <bretcj1@gmail.com>
2023-02-04 01:47:36 -0500

    Added a favicon
    Added a favicon to the page

---
+++ public/javascript/join.js
@@ -0,0 +1,7 @@
+function submitEvent() {
+    var user = {
+        code: document.getElementsByName("jCode")[0].value,
+        name: document.getElementsByName("jName")[0].value,
+        home: document.getElementsByName("jHome")[0].value
+    };
+}
